ea825dc4257e4bbfecf0343553e4206ebb89debd
yoad100 <yoad.gadot@gmail.com>
2025-09-22 11:41:15 +0300

    added comment

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,38 +1,43 @@
-import axios, { AxiosInstance } from 'axios';
+  import axios, { AxiosInstance } from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://localhost:7115/api';
+  const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://localhost:7115/api';
+  /**
+   * BaseService is an abstract generic class that provides common
+   * CRUD-like methods to interact with a REST API endpoint.
+   * 
+   * T = DTO type used when sending data to the API
+   * R = Model type returned by the API
+   */
+  export abstract class BaseService<T, R> {
+    protected api: AxiosInstance;
 
-export abstract class BaseService<T, R> {
-  protected api: AxiosInstance;
+    constructor(protected endpoint: string) {
+      this.api = axios.create({
+        baseURL: API_BASE_URL,
+        timeout: 10000,
+        headers: { 'Content-Type': 'application/json' },
+      });
+      // Log errors if API fails
+      this.api.interceptors.response.use(
+        (response) => response,
+        (error) => {
+          console.error(`API Error [${endpoint}]:`, error);
+          return Promise.reject(error);
+        }
+      );
+    }
 
-  constructor(protected endpoint: string) {
-    this.api = axios.create({
-      baseURL: API_BASE_URL,
-      timeout: 10000,
-      headers: { 'Content-Type': 'application/json' },
-    });
-    // Log errors if API fails
-    this.api.interceptors.response.use(
-      (response) => response,
-      (error) => {
-        console.error(`API Error [${endpoint}]:`, error);
-        return Promise.reject(error);
-      }
-    );
+    async getAll(): Promise<R[]> {
+      const res = await this.api.get<R[]>(`/${this.endpoint}`);
+      return res.data || [];
+    }
+
+    async delete(id: string): Promise<void> {
+      await this.api.delete(`/${this.endpoint}/${id}`);
+    }
+
+    protected async saveBulk(data: T[]): Promise<R[]> {
+      const res = await this.api.post<R[]>(`/${this.endpoint}/save`, data);
+      return res.data || [];
+    }
   }
-
-  async getAll(): Promise<R[]> {
-    const res = await this.api.get<R[]>(`/${this.endpoint}`);
-    return res.data || [];
-  }
-
-  async delete(id: string): Promise<void> {
-    await this.api.delete(`/${this.endpoint}/${id}`);
-  }
-
-  protected async saveBulk(data: T[]): Promise<R[]> {
-    debugger;
-    const res = await this.api.post<R[]>(`/${this.endpoint}/save`, data);
-    return res.data || [];
-  }
-}
